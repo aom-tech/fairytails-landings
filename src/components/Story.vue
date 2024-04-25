f0e03b73123387c24611661faa4bdf247cebaa3c
Yaroslav Yudinskikh <y.yudinskih@innopolis.ru>
2024-04-25 11:41:50 +0300

    add case with choices

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -8,21 +8,16 @@ const { content } = defineProps({ content: Array })
 const levels = ref([content])
 
 const onSelected = (idx, newContent) => {
-    console.log('onSelected p', idx, newContent)
     if (levels.value.length > idx + 1) {
         levels.value.splice(idx + 1, levels.value.length - idx, newContent)
-        // levels.value[idx] = newContent
     } else {
         levels.value.push(newContent)
     }
 }
-
-console.log('content', content)
 </script>
 
 <template>
     <template :id="idx" v-for="(level, idx) in levels" :key="idx" >
-        -------Level: {{ idx }}
-        <StoryLevel :content="level" @select="(v) => onSelected(idx, v)" />
+        <StoryLevel :level="idx" :content="level" @select="(v) => onSelected(idx, v)" />
     </template>
 </template>
--- src/components/StoryLevel.vue
+++ src/components/StoryLevel.vue
@@ -1,14 +1,11 @@
 <script setup>
-import { defineProps, toRefs, toRaw } from 'vue'
+import { defineProps } from 'vue'
 
-const { content } = defineProps({ content: Array })
+const { content, level } = defineProps({ content: Array, level: Number })
 
 const emit = defineEmits(['select'])
 
-
-
-const onSelected = (idx, newContent) => {
-    console.log('onSelected c', newContent)
+const onSelected = (newContent) => {
     emit('select', newContent)
 }
 </script>
@@ -16,19 +13,110 @@ const onSelected = (idx, newContent) => {
 <template>
     <div>
         <template v-for="item in content">
-                <div v-if="typeof item === 'object'">
-                    <div v-for="(key, idx) in Object.keys(item)">
-                        <input id="tab-btn-1" name="tab-btn" type="radio" value="" @change="(v) => onSelected(v, item[key])">
-                        <label for="tab-btn-1">{{ idx }}. {{key}}</label>
-                        <!-- <i>{{ item[key] }}</i> -->
-                    </div>
-                </div>
-                <p v-if="typeof item === 'string'">{{ item }}</p>
-                <p v-else-if="typeof item === 'string'">{{ item }}</p>
+            <template v-if="typeof item === 'object'">
+            <form  class="flex flex-col items-start space-y-2">
+                Выбор:
+                <template v-for="(key, idx) in Object.keys(item)">
+                    <input :id="`option-${level}-radio-${idx}`" name="tab-btn" type="radio" @change="(v) => onSelected(item[key])">
+                    <label :for="`option-${level}-radio-${idx}`">{{ idx }}. {{key}}</label>
+                </template>
+            </form>
+        </template>
+        <template v-else-if="typeof item === 'string' && item.includes('/')">
+            <img >{{ item }}</img>
+        </template>
+        <template v-else >
+            <p class="py-3">{{ item }}</p>
+        </template>
         </template>
     </div>
 </template>
 
-<style>
+<style >
+.choice-tabs {
+        padding: 10px 0;
+    }
 
+    .tab > input[type="radio"] {
+        display: none;
+    }
+
+    .tab > input[type="radio"]:checked + label {
+        @apply border-ochre text-ochre
+    }   
+    
+    .tab {
+        padding: 10px;
+        @apply border border-coal;
+    }
+
+    .tab:hover {
+        @apply border-ochre text-ochre
+    }
+
+    .tab:not(:first-child) {
+        @apply mt-2;
+    }
+
+    .tab:has(input[type="radio"]:checked) {
+        @apply border-ochre text-ochre
+    }
+
+    input[type="radio"] {
+        display: none;
+    }
+
+    input[type="radio"] + label:before {
+        content: "";
+        @apply h-5 w-5 border-ochre border rounded-full;
+    }
+
+    input[type="radio"]:checked + label:before {
+        @apply h-5 w-5 bg-ochre rounded-full;
+    }
+
+    input[type="radio"] + label {
+        position: relative;
+        cursor: pointer;
+        display: inline-flex;
+        align-items: center;
+        gap: 0.8em;
+        
+        @apply border border-ochre text-ochre rounded-lg px-4 py-3;
+    }
+
+    /* input[type="radio"] {
+  display: none;
+}
+label {
+  position: relative;
+  color: #4189e0;
+  font-family: "Poppins", sans-serif;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  gap: 0.8em;
+  border: 3px solid #4189e0;
+  padding: 1em 2em;
+  border-radius: 0.5em;
+}
+label:before {
+  content: "";
+  height: 2em;
+  width: 2em;
+  border: 3px solid #4189e0;
+  border-radius: 50%;
+}
+
+input[type="radio"]:checked + label:before {
+  height: 1em;
+  width: 1em;
+  border: 0.65em solid #ffffff;
+  background-color: #4189e0;
+}
+
+input[type="radio"]:checked + label {
+  background-color: #4189e0;
+  color: #ffffff;
+} */
 </style>
